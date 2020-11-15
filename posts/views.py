64a7c60a382b214e8cc07cc8eb6b6e0f478e70be
al <alxayeed@gmail.com>
2020-11-15 15:31:20 +0600

    pagination added to blog-listing

--- posts/views.py
+++ posts/views.py
@@ -1,17 +1,36 @@
 from django.shortcuts import render
 import requests
+from django.core.paginator import Paginator
 
 # POSTS VIEW ENDPOINT
 
 
 def posts(request):
+    '''
+    View for homapage/ all posts
+    '''
     response = requests.get(
         'https://jsonplaceholder.typicode.com/posts').json()
+
+    # Pagination
+    paginator = Paginator(response, 10)
+    page_number = request.GET.get('page')
+
+    # if it is the first page, get response from page 1
+    if page_number is None:
+        response = paginator.page(1)
+    # else, get posts of page_number
+    else:
+        response = paginator.page(page_number)
+
     return render(request, 'blog-listing.html', {'response': response})
 
 
 # POST DETAILS VIEW ENDPOINT
 def post_details(request, post_id):
+    '''
+    View for details of a post
+    '''
     url = 'https://jsonplaceholder.typicode.com/posts/'+str(post_id)
     post = requests.get(url).json()
 
