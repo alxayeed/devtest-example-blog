5ea64459b39d8d0a0f32fed569d5874b50737273
al <alxayeed@gmail.com>
2020-11-18 16:32:07 +0600

    api created for comments.auth api restructered

--- backend/grayspaceit/urls.py
+++ backend/grayspaceit/urls.py
@@ -3,10 +3,13 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from posts.views import posts
+from posts import api
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
     path('posts/', include('posts.urls')),
     path('', include('posts.urls')),
+    path('api/comments/', include('posts.api.urls')),
+    path('api/auth/', include('authentication.api.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
